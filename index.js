777b18e86d896f9f0a14173321f7d33711ded5f4
Chanuka999 <chanukaranditha99@gmail.com>
2024-08-27 19:05:57 -0700

    js code apply

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    candyCrushGame();
+})
+
+function candyCrushGame(){
+    const grid = document.querySelector(".grid");
+    const scoreDisplay = document.getElementById("Score");
+    const width = 0;
+    const squares = [];
+    let Score = 0;
+}
